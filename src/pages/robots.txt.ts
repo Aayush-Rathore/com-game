5c7598910d81e27c90980949f16698593c877e62
Aayush Rathore <aayu.r.2003@gmail.com>
2026-03-16 14:59:53 +0530

    🔍 Technical SEO improvement

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,11 +1,13 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ site }) => {
+	const sitemapUrl = new URL('sitemap-index.xml', site).toString();
+
   const robots = `
 User-agent: *
 Allow: /
 
-Sitemap: ${site}sitemap-index.xml
+Sitemap: ${sitemapUrl}
 `;
 
   return new Response(robots.trim(), {
