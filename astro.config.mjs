751ea0af10219fc6337364880f17300c7c426df6
Aayush Rathore <aayu.r.2003@gmail.com>
2026-07-02 05:25:49 +0530

    Blogs updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
@@ -9,6 +9,22 @@ export default defineConfig({
   site: "https://www.comegameapp.com",
   devToolbar: {
     enabled: false,
+  },
+
+  env: {
+    schema: {
+      PUBLIC_SITE_URL: envField.string({
+        context: "client",
+        access: "public",
+        default: "https://www.comegameapp.com",
+      }),
+      PUBLIC_AFFILIATE_URL: envField.string({
+        context: "client",
+        access: "public",
+        default:
+          "https://web-in.comegamehub.com/en/affiliate-invited?c=5DAGYXA3&s=3",
+      }),
+    },
   },
 
   vite: {
